39a43e1ecf6774f0c86e2a443e4248cf6149731f
Jakub Gurba <gurbaj@office.wit.edu.pl>
2022-11-22 00:26:10 +0100

    zmiany

--- src/main/java/org/example/pl/edu/wit/Airport.java
+++ src/main/java/org/example/pl/edu/wit/Airport.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 @Data(staticConstructor = "create")
 public class Airport {
-    private final String city;
+    private final String cit
     private final BigDecimal airfare;
     private final String name;
     private final String code;
